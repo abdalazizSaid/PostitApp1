316d9e5c85c7b998000767a2497fccd76f247fe6
Abdalaziz <66j1948@utas.edu.om>
2025-11-19 23:16:49 +0400

    sub4

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,7 +1,11 @@
 const About = () => {
   return (
     <div>
-      <b>Abdalaziz - Student 2025</b>
+      <b>
+        Abdalaziz - Student 2025 client link
+        https://postitapp1-y1dm.onrender.com server link
+        https://postitapp1-server.onrender.com
+      </b>
     </div>
   );
 };
